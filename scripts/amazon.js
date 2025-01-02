8d8cf9c07524d032e448b111ddbbce4bdb96d52c
quinnhu99 <quynhnhu.pt99@gmail.com>
2025-01-02 20:27:22 +0700

    User product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,4 @@
-const products = [{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        start: 4,
-        count: 5,
-    },
-    priceCents: 799
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        start: 4,
-        count: 127.99,
-    },
-    priceCents: 2195
-},
-{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 10 Pairs',
-    rating: {
-        start: 4.5,
-        count: 87,
-    },
-    priceCents: 1090
-}];
-
 let productHTML = '';
-
 
 products.forEach((product) => {
    productHTML += `
@@ -43,7 +14,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.start *10}.png">
+              src="images/ratings/rating-${product.rating.stars *10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
